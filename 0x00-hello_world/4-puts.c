4df31c868e699d8f39d3f238c540349eefd6e9d5
Haile-Baranto <hailebaranto@gmail.com>
2022-12-08 16:25:36 -0800

    add 4-puts.c file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+/** prints "Programming is like building a multilingual puzzle with new line*/
+
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle\n");
+return (0);
+}
