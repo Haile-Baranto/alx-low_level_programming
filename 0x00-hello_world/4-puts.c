65eba1b2cc7b619af81bb86c0f7c81a777930a02
Haile-Baranto <hailebaranto@gmail.com>
2022-12-08 17:15:02 -0800

    edit 4-puts.c a gain

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,12 @@
 #include <stdio.h>
 
 /**
- *
- * main entry point
- *
- * prints "Programming is like building a multilingual puzzle with new line
+ * main - Entry point
  * 
+ * Description: 'the program's description'
+ *
  * Return: Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle \n");
