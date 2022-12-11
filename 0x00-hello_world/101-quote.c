050e24544fbb05334c52177985e369eb16cf724a
Haile-Baranto <hailebaranto@gmail.com>
2022-12-11 19:49:38 +0300

    Add 101-quot.c file

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#define MAXSTRING 100
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ *The program prints out the string array using puthcar
+ *
+ */
+
+int main(void)
+{
+	char array[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	for (int char_seq = 0; char_seq < MAXSTRING; char_seq++)
+	{
+		if (array[char_seq] == '\0')
+		{
+			putchar('\n');
+			break;
+		}
+		else
+			putchar(array[char_seq]);
+	}
+	return (0);
+}
+
