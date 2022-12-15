dfbbef5f9574e3fb2e80619cda973b71808bb809
Haile Baranto <hailebaranto@gmail.com>
2022-12-15 17:01:08 +0300

    add 2-mul.c file

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+/**
+ * mul - return multiplication of two integers
+ * @a: is a parameter used to pass integer to func mul
+ * @b: the same as @a
+ * Return: result of multiplication (int type)
+ */
+
+int mul(int a, int b)
+{
+	int result;
+
+	result = a * b;
+	return (result);
+}
+
