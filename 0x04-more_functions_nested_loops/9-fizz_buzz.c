71c0b42f46f449ff99ab2c00da20b273feb7c484
Haile Baranto <hailebaranto@gmail.com>
2022-12-15 21:39:29 +0300

    add 9-fizz_buzz.c file

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,28 @@
+#include "main.h"
+/**
+ * main - prints 1 -100 but for multiple 3 print "Fizz"
+ * for multiple of 5 Buz for multiple of both 3 and 5
+ * FizzBuzz
+ * standard library allowed
+ * Return: 0
+ */
+
+int main(void)
+{
+	int num;
+
+	num = 1;
+	for (num = 1; num <= 100; num++)
+	{
+		if ((num % 3 != 0) && (num % 5 != 0))
+			printf("%i ", num);
+		else if ((num % 3 == 0) && (num % 5 == 0))
+			printf("FizzBuzz ");
+		else if (num % 3 == 0)
+			printf("Fizz ");
+		else if (num % 5 == 0)
+			printf("Buzz ");
+	}
+	printf("\n");
+	return (0);
+}
