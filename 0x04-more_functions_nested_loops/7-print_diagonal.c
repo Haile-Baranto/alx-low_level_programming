f172999dd16ec22b9b09334993856333ddd993bb
Haile Baranto <hailebaranto@gmail.com>
2022-12-15 20:06:34 +0300

    add 7-print_diagonal.c file

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_diagonal - the function draws diagonal using '\&
+ * using only _putchar function
+ * @n: parameter that recive the number of \$ to printed
+ * Return: void
+ */
+
+void print_diagonal(int n)
+{
+	 int  count1;
+	 int count2;
+
+	count1 = 1;
+	count2 = 1;
+	if  (n > 0)
+	{
+		for (count1 = 1; count1 <= n; count1++)
+		{
+			for (count2 = count1; count2 >= 1; count2--)
+			{
+				_putchar(' ');
+				
+			}
+			_putchar('\\');
+			_putchar('\n');
+		}
+	}
+
+	else
+		_putchar('\n');
+}
