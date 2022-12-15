3422f00db4a22c3ece53b0d0b72c9facacfc539c
Haile Baranto <hailebaranto@gmail.com>
2022-12-15 19:04:26 +0300

    add 6-print_line.c file

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_line - the function draws a straight line using '_'
+ * in the terminal using only putchar
+ * @n: parameter that recive the number of '_' to printed
+ * Return: void
+ */
+
+void print_line(int n)
+{
+	char count;
+
+	count = 1;
+	if  (n > 0)
+	{
+		for (count = 1; count <= n; count++)
+		{
+			_putchar('-');
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
