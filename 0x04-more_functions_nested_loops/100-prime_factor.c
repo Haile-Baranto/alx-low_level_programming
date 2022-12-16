7ca6e2e3f66caeb5a72409e52efe3a5c782d57b2
Haile Baranto <hailebaranto@gmail.com>
2022-12-16 06:43:11 +0300

    add 100-prime_factor.c file

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,4 +1,5 @@
-#include "main.h"
+#include <stdio.h>
+#include <math.h>
 /**
  * main - find and print the largest prime factor
  * of 612852475143
@@ -12,13 +13,13 @@ int main(void)
 	long int result;
 
 	num = 612852475143;
-	for (prime_factor = 1; prime_factor < num; prime_factor += 2 )
+	for (prime_factor = 2; prime_factor < (int)sqrt(num); prime_factor += 1)
 	{
-		if (num % prime_factor == 1)
+		if (num % prime_factor == 0)
+		{
 			result = prime_factor;
+		}
 	}
-	if (prime_factor == 1)
-		printf("THe number does not have prime factor");
 	printf("%li\n", result);
 	
 	return (0);
