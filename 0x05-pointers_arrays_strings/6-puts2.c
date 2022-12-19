d345a7a1313b05c665cac5f1651ac2deee753ab1
Haile-Baranto <hailebaranto@gmail.com>
2022-12-19 16:21:56 +0300

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts2 - prints every oother characheter
+ * @str: pointer parameter which points to character sequence to
+ * be printed out
+ */
+
+void puts2(char *str)
+{
+	int len;
+
+	for ( len = 0; str[len] != '\0'; len += 2)
+	{
+		_putchar(str[len]);
+	}
+	_putchar('\n');
+}
