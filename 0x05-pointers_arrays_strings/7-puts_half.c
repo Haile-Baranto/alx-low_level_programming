10bab7598c4b9a403a866e737fda52b17ce0d2a9
Haile-Baranto <hailebaranto@gmail.com>
2022-12-19 16:53:55 +0300

    edit betty style

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -9,13 +9,13 @@
 void puts_half(char *str)
 {
 	int length;
-        int counter;
+	int counter;
 
-        length = 0;
-        for (length = 0; str[length] != '\0'; length++)
-        {
-                length += 1;
-        }
+	length = 0;
+	for (length = 0; str[length] != '\0'; length++)
+	{
+		length += 1;
+	}
 	for (counter = length / 2; str[counter] != '\0'; counter += 1)
 	{
 		_putchar(str[counter]);
