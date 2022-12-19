54998e3245b0ac063f72e622092cd8befca5067a
Haile-Baranto <hailebaranto@gmail.com>
2022-12-19 14:00:58 +0300

    add 4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_rev - prints out string of charachters to stdio in
+ * in reverse order
+ * @s: the address of the string to be printed out
+ * Description: can use only _putchar
+ */
+
+void print_rev(char *s)
+{
+	int length;
+	int counter;
+
+	for (length = 1; s[length] != '\0'; length++)
+	{
+		length += 1;
+	}
+	length = length -1;
+	for (counter = length; counter >= 0; counter--)
+	{
+		_putchar(s[counter]);
+	}
+	_putchar('\n');
+}
