84c945e4c6f5acadbeb8555b51431c8e6e28e5ec
Haile-Baranto <hailebaranto@gmail.com>
2022-12-22 15:18:11 +0300

    edit betty style

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,29 +1,21 @@
 #include "main.h"
 
 /**
- * _strcat - concatinates two strings
+ * *_strcat - concatinates two strings
  * @dest: is where the charachters from src will be concatinated
  * @src: from where we will take the strings to be concatinated
- * Reterun: resulting string
+ * Return:  string of charachters
  * Description: using library function is not allowed except _puthcar()
  */
 
 char *_strcat(char *dest, char *src)
 {
-	/*
-	char* ptr1;
-	char* ptr2;
-	*/
 	int len;
 	int j;
 
-	/*
-	ptr1 = dest;
-	ptr2 = src;
-	*/
 	j = 0;
 	len = 0;
-	while(dest[len] != '\0')
+	while (dest[len] != '\0')
 	{
 		len++;
 	}
