c1f4ab9e51b9b9d2d10ee037803e7e87dc36f859
Haile-Baranto <hailebaranto@gmail.com>
2022-12-22 20:35:01 +0300

    edit betty style of 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -15,10 +15,10 @@ int _strcmp(char *s1, char *s2)
 	int index;
 
 	index = 0;
-	for (index = 0; *(s1 + index) != '\0' || *(s2 + index) != 0; index++)
+	for (index = 0; s1[ndex] != '\0' || s2[index] != 0; index++)
 	{
-		if (*(s1 + index) != *(s2 + index))
-			return (*(s1 + index) - *(s2 + index));
+		if (s1[index] != s2[index])
+			return (s1[index] - s2[index]);
 	}
 	return (0);
 }
