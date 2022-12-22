9526c3bd5ee79a103ba2086183fef4b8b22490a1
Haile-Baranto <hailebaranto@gmail.com>
2022-12-22 20:37:57 +0300

    edit syntax and betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
 /**
  * _strcmp - compares two strings
  * @s1: pointer to the first string
- * @s2 pointer to the first string
+ * @s2: pointer to the first string
  * Return: 1 if the first non maching charachetrs of string one is
  * in ascci greater than that of s2. 0 if they are equal and -1 if
  * s1 is greater
@@ -15,7 +15,7 @@ int _strcmp(char *s1, char *s2)
 	int index;
 
 	index = 0;
-	for (index = 0; s1[ndex] != '\0' || s2[index] != 0; index++)
+	for (index = 0; s1[index] != '\0' || s2[index] != 0; index++)
 	{
 		if (s1[index] != s2[index])
 			return (s1[index] - s2[index]);
