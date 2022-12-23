ae2ba88d47a9302cc988be430046a4eaa076d6a3
Haile-Baranto <hailebaranto@gmail.com>
2022-12-23 22:42:14 +0300

    add 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_number - prints the arguement out
+ * @n: parameter to be printed out
+ */
+
+void print_number(int n)
+{
+	if (n == 0)
+		_putchar('0');
+	if (n / 10 != 0)
+	{
+		print_number(n / 10);
+		if (n > 0)
+		{
+			_putchar(n % 10 + '0');
+		}
+		else
+		{
+			_putchar(-n % 10 + '0');
+		}
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	{
+		_putchar(n % 10 + '0');
+	}
+	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+	{
+		n = -n;
+		_putchar('-');
+		_putchar(n % 10 + '0');
+	}
+}
