ce2169840d2b85f5b9a8123facab0b06e630a63a
Haile-Baranto <hailebaranto@gmail.com>
2022-12-23 05:46:59 +0300

    edit 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,27 +11,29 @@ char *cap_string(char *str)
 	int scape;
 	int temp;
 	char special_symbols[] = ",;.!?(){}\n\t\" ";
-	temp = 1
+	temp = 0
 		;
 	for (index = 0; str[index] != '\0'; index++)
 	{
 		if (str[0] > 96 && str[0] < 123)
-			temp = 0;
+			temp = 1;
 		for (scape = 0; special_symbols[scape] != '\0'; scape++)
 		{
 			if (special_symbols[scape] == str[index])
-				temp = 0;
+				temp = 1;
 		}
 
-		if (!temp)
+		if (temp)
 		{
 			if (str[index] > 96 && str[index] < 123)
 			{
 				str[index] -= 32;
-				temp = 1;
+				temp = 0;
 			}
+			else if (str[index] == '\t')
+				str[index] = ' ';
 			else if (str[index] > 64 && str[index] < 91)
-				temp = 1;
+				temp = 0;
 			else if (str[index] > 47 && str[index] < 58)
 				temp = 0;
 		}
