2b3b4eb6a735255b28d035d1a3350c396064f6d5
Haile-Baranto <hailebaranto@gmail.com>
2022-12-23 22:50:44 +0300

    edit 101-print_numbers.c

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -7,8 +7,6 @@
 
 void print_number(int n)
 {
-	if (n == 0)
-		_putchar('0');
 	if (n / 10 != 0)
 	{
 		print_number(n / 10);
@@ -21,11 +19,11 @@ void print_number(int n)
 			_putchar(-n % 10 + '0');
 		}
 	}
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n > 0))
+	else if ((n / 10 == 0) && (n >= 0))
 	{
 		_putchar(n % 10 + '0');
 	}
-	else if ((n / 10 == 0) && (n % 10 != 0) && (n <= 0))
+	else if ((n / 10 == 0) && (n < 0))
 	{
 		n = -n;
 		_putchar('-');
