6125bd6d223eb5c565db995e1f2757f31ab6cec6
Haile-Baranto <hailebaranto@gmail.com>
2023-01-02 22:53:55 +0300

    edit 8-print_diagsums.c

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -9,16 +9,23 @@
 void print_diagsums(int *a, int size)
 {
 	int i;
+	int j;
 	int main_diagonal;
 	int off_diagonal;
 
 	main_diagonal = 0;
+	off_diagonal = 0;
 	for (i = 0; i < size * size; i++)
 	{
 		if (i % (size + 1) == 0)
 			main_diagonal += a[i];
-		if (i % (size - 1) == 0 && i != (size * size - 1) && i != 0)
-			off_diagonal += a[i];
+	}
+	
+	
+	for (j = 0; j < size * size; j++)
+	{
+		if (j % (size - 1) == 0 && j != (size * size - 1) && j != 0)
+			off_diagonal += a[j];
 	}
 	printf("%d, %d\n", main_diagonal, off_diagonal);
 }
