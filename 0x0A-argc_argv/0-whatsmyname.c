e82762f12d3a6ab422450018b51f56dbf54be7bf
Haile-Baranto <hailebaranto@gmail.com>
2023-01-06 10:21:57 +0300

    edit betty of 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,6 +2,8 @@
 /**
  * main - prints its name followed by its name.
  * Return: 0 if the program is successfull.
+ * @argc: the number of arguement from command linepassed to the program
+ * @argv: poiter to to an array of string
  * Description: If you rename the program, it will print the new name,
  * without having to compile it again. and
  * You should not remove the path before the name of the program
