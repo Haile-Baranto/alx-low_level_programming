16bcf5869323a3933ea870388c0b2aa0de93990f
Haile-Baranto <hailebaranto@gmail.com>
2023-01-06 09:40:12 +0300

    add all files

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - prints its name followed by its name.
+ * Return: 0 if the program is successfull.
+ * Description: If you rename the program, it will print the new name,
+ * without having to compile it again. and
+ * You should not remove the path before the name of the program
+ */
+
+int main(argc, **argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
