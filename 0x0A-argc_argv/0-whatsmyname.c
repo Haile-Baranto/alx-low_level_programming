b2706c2ed595e17fc4fa12c0435d84b5bb182c7f
Haile-Baranto <hailebaranto@gmail.com>
2023-01-06 09:45:35 +0300

    add 0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,8 +7,9 @@
  * You should not remove the path before the name of the program
  */
 
-int main(argc, **argv)
+int main(int argc, char **argv)
 {
-	printf("%s\n", argv[0]);
+	argc = 0;
+	printf("%s\n", argv[argc]);
 	return (0);
 }
