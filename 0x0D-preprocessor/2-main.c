a415581cb12dc4f83fbf7187fa2fde57d92965e1
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 12:15:18 +0300

    add 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include "stdio.h"
+/**
+ * main - prints the name of the file followed by new line
+ * Return: 0 if successful else 1
+ * FILE: 2-main.c
+ * Auth: Haile W Baranto
+ * Descripiton - using std library is allowed
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
