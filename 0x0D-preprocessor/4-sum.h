51d9f05e1a8efbe0424a5a690f4c2cb3c50443cd
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 13:12:23 +0300

    add 4-sum.h

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,9 @@
+#ifndef FILE
+#define FILE
+#define SUM(x, y) ((x) + (y))
+/**
+ * FILE: 4-sum.h
+ * ABS(X):function-like macro that computes the sum of its two arguements
+ * Auth: Haile W Baranto
+ */
+#endif
