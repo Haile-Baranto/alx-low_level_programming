f95dd962b176bcdabec3d6d263aae1fafeb23077
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 13:07:07 +0300

    add 3-function_like_macro.h

---
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -0,0 +1,9 @@
+#ifndef FILE
+#define FILE
+#define ABS(X) ((X) >= 0 ? (X) : (-X))
+/**
+ * FILE: 3-function_like_macro.h
+ * ABS(X):function-like macro that computes the absolute value of a number x
+ * Auth: Haile W Baranto
+ */
+#endif
