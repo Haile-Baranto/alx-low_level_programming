0d8b2592b086480ea34f3e876e75603c3d8517cd
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 11:57:11 +0300

    add 1-pi.h

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,9 @@
+/**
+ * FILE: 1-pi.h
+ * Desc:  defines a macro named PI as an abbreviation for the token 3.14159265359
+ * Auth: Haile W Baranto
+ */
+#ifndef PI 
+#define PI 3.14159265359
+#endif
+
