ba95cafb44cfd5024cd7e5d54ab35019f352696d
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 12:07:31 +0300

    edit betty style of 1-pi.h

--- 0x0D-preprocessor/1-pi.h
+++ 0x0D-preprocessor/1-pi.h
@@ -1,9 +1,10 @@
+#ifndef PI
+#define PI 3.14159265359
 /**
  * FILE: 1-pi.h
- * Desc:  defines a macro named PI as an abbreviation for the token 3.14159265359
+ * Desciption: defines a macro named PI as an abbreviation
+ * for the token 3.14159265359
  * Auth: Haile W Baranto
  */
-#ifndef PI 
-#define PI 3.14159265359
 #endif
 
