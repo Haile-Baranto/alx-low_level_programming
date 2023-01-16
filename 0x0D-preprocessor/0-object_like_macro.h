6855d0738636228cb7508fb864936fef64cec0f1
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 11:57:57 +0300

    edit 0-object_like_macro.h

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -3,9 +3,6 @@
  * Descripton: defines a macro named SIZE as an abbreviation for the token 1024
  * Auth: Haile W Baranto
  */
-#ifndef HEADER_FILE 
-#define HEADER_FILE 
-#include "0-object_like_macro.h" 
-#endif
-
+#ifndef SIZE 
 #define SIZE 1024
+#endif
