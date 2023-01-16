be16d28d4c4d3cf9fb2d2811a49b0d99c0ccb272
Haile-Baranto <hailebaranto@gmail.com>
2023-01-16 16:48:55 +0300

    add dog.h

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_HEADER
+#define DOG_HEADER
+
+/**
+ * struct dog - Define a struct with the ff members
+ * @name: First member of type pointer to char
+ * @age: Second member of type int
+ * @owner: Third member of type pointer to char
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
+
