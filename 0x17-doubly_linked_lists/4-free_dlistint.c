fb1c602986038e64677ac3be19dcc5202014f848
Haile-Baranto <hailebaranto@gmail.com>
2023-03-16 23:42:46 +0300

    Add soln to 4-free_dlistint.c

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+  * free_dlistint - Frees a dlistint_t list.
+  * @head: The head of the dlistint_t list.
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *trav;
+
+	while (head)
+	{
+		trav = head->next;
+		free(head);
+		head = trav;
+	}
+}
