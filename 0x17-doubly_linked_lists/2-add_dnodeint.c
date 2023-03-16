9c708bc4f2b050b7e7ca84882b477c590914a4ce
Haile-Baranto <hailebaranto@gmail.com>
2023-03-16 23:12:13 +0300

    Add soln to 2-add_dnodeint.c

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,22 @@
+#include "lists.h"
+/**
+ * add_dnodeint - Adds node at the begging of a dlistint_t list.
+ * @head: A double pointer to dlistint_t list
+ * @n: A vaue to be inserted at the beggining of dlistint_t list
+ * Return: the address of the new element, or NULL if it failed
+ */
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+	
+	new = malloc(sizeof(dlistint_t));
+	if (new == NULL)
+		return NULL;
+	new->n = n;
+	new->prev = NULL;
+	new->next = (*head);
+	if (*head != NULL)
+		(*head) -> prev = new;
+	(*head) = new;
+	return (new);
+}
